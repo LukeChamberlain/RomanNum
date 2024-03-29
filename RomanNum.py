c55525d112bda1fda0ed3a1ed0eb71eb979c87d9
LukeChamberlain <cha22044@byui.edu>
2024-03-29 08:29:55 -0600

    Convert the Roman Numerals to Numbers

--- RomanNum.py
+++ RomanNum.py
@@ -1,3 +1,40 @@
 RomanNum = input("Please type a roman Numeral (XVI, IIVL, XLIV, etc): ")
 RomanNumSplit = list(RomanNum)
+# def Convert(RomanNumSplit):
+count = len(RomanNumSplit)
+index = 0
+print(len(RomanNumSplit))
+def Convert(RomanNumSplit):
+    index = 0
+    while index < len(RomanNumSplit):
+        if RomanNumSplit[index] == "I":
+            RomanNumSplit[index] = 1
+            index += 1
+        elif RomanNumSplit[index] == "V":
+            RomanNumSplit[index] = 5
+            index += 1
+        elif RomanNumSplit[index] == "X":
+            RomanNumSplit[index] = 10
+            index += 1
+        elif RomanNumSplit[index] == "L":
+            RomanNumSplit[index] = 50
+            index += 1
+        elif RomanNumSplit[index] == "C":
+            RomanNumSplit[index] = 100
+            index += 1
+        elif RomanNumSplit[index] == "D":
+            RomanNumSplit[index] = 500
+            index += 1
+        elif RomanNumSplit[index] == "M":
+            RomanNumSplit[index] = 1000
+            index += 1
+        else:
+            print("Please input a valid Roman Numeral")
+            break
+        
+Convert(RomanNumSplit)
+print(RomanNumSplit)
+        
+# Convert(RomanNumSplit)
+# print(RomanNumSplit)
 
