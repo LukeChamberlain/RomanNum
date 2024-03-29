43c1e7516bce015acfeb645bd3d93c9eb3d384c3
LukeChamberlain <cha22044@byui.edu>
2024-03-29 09:57:35 -0600

    Finished conversion to Number

--- RomanNum.py
+++ RomanNum.py
@@ -1,47 +1,25 @@
+def parseRoman(roman):
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    parsed = [roman_numerals.get(char, -1) for char in roman]
+    if -1 in parsed:
+        raise ValueError("Invalid Roman numeral")
+    return parsed
 
-intial = input("Please type a roman Numeral (XVI, IIVL, XLIV, etc): ")
-RomanNumSplit = list(intial)
-count = len(RomanNumSplit)
-
-def RomToNum(RomanNumSplit):
-    index = 0
-    while index < len(RomanNumSplit):
-        if RomanNumSplit[index] == "I":
-            RomanNumSplit[index] = 1
-            index += 1
-        elif RomanNumSplit[index] == "V":
-            RomanNumSplit[index] = 5
-            index += 1
-        elif RomanNumSplit[index] == "X":
-            RomanNumSplit[index] = 10
-            index += 1
-        elif RomanNumSplit[index] == "L":
-            RomanNumSplit[index] = 50
-            index += 1
-        elif RomanNumSplit[index] == "C":
-            RomanNumSplit[index] = 100
-            index += 1
-        elif RomanNumSplit[index] == "D":
-            RomanNumSplit[index] = 500
-            index += 1
-        elif RomanNumSplit[index] == "M":
-            RomanNumSplit[index] = 1000
-            index += 1
+def convert(parsed_roman):
+    A = 0
+    B = 0
+    for value in parsed_roman:
+        if value > B:
+            A += value - 2 * B
         else:
-            print("Please input a valid Roman Numeral")
-            break
-def RuleOne(RomanNumSplit):
-    val = 1
-    while True:
-        while val >= len(RomanNumSplit):
-            if RomanNumSplit[0] >= RomanNumSplit[val]:
-                val += 1
-                return True
-            else:
-                return False
-        return sum(RomanNumSplit)
+            A += value
+        B = value
+    return A
 
-RomToNum(RomanNumSplit)
-print(RuleOne(RomanNumSplit))
-
-
+try:
+    roman_numeral = input("Please type a Roman Numeral (XVI, IIVL, XLIV, etc): ").upper()
+    parsed_roman = parseRoman(roman_numeral)
+    decimal_value = convert(parsed_roman)
+    print("Value Number:", decimal_value)
+except ValueError as e:
+    print(e)
