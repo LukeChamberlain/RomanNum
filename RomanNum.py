00c5c32a844ddca8a38d1f7d4e3dd2b0869f3f5b
LukeChamberlain <cha22044@byui.edu>
2024-03-29 10:16:43 -0600

    Finished Converting it Back

--- RomanNum.py
+++ RomanNum.py
@@ -1,14 +1,14 @@
 def parseRoman(roman):
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    parsed = [roman_numerals.get(char, -1) for char in roman]
+    romanNumerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    parsed = [romanNumerals.get(char, -1) for char in roman]
     if -1 in parsed:
         raise ValueError("Invalid Roman numeral")
     return parsed
 
-def convert(parsed_roman):
+def convert(parsedRoman):
     A = 0
     B = 0
-    for value in parsed_roman:
+    for value in parsedRoman:
         if value > B:
             A += value - 2 * B
         else:
@@ -16,10 +16,21 @@ def convert(parsed_roman):
         B = value
     return A
 
+def parseBack(nums):
+    romanNumerals = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
+    roman = ""
+    for num in nums:
+        for key in sorted(romanNumerals.keys(), reverse = True):
+            while num >= key:
+                roman += romanNumerals[key]
+                num -= key
+    return roman
+
+        
 try:
-    roman_numeral = input("Please type a Roman Numeral (XVI, IIVL, XLIV, etc): ").upper()
-    parsed_roman = parseRoman(roman_numeral)
-    decimal_value = convert(parsed_roman)
-    print("Value Number:", decimal_value)
+    romanNumeral = input("Please type a Roman Numeral (XVI, IIVL, XLIV, etc): ").upper()
+    parsedRoman = parseRoman(romanNumeral)
+    print("Number: ", convert(parsedRoman))
+    print("Roman Numeral: ", parseBack(parsedRoman))
 except ValueError as e:
     print(e)
